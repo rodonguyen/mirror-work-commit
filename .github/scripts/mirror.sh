2bad105d1c1690a824352a4a241894da2d5c8a85
Rodo <rodonguyendd@gmail.com>
2025-06-09 14:22:57 +1000

    chore: finalise

--- .github/scripts/mirror.sh
+++ .github/scripts/mirror.sh
@@ -17,14 +17,12 @@ git config user.email "$AUTHOR_EMAIL"
 echo "Configuring git authentication..."
 git remote set-url origin "https://x-access-token:${PERSONAL_PAT}@github.com/rodonguyen/mirror-work-commit.git"
 
+# --------------------------------------------
 # 1. Fetch current commit count from GitHub API
-echo ""
 echo "Fetching commit count from GitHub..."
 SINCE=$(date -u -d "24 hours ago" +"%Y-%m-%dT%H:%M:%SZ")
 NOW=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 echo "Fetching commits from $SINCE to $NOW"
-
-# --------------------------------------------
 
 Q="author:$WORK_LOGIN committer-date:$SINCE..$NOW"
 ENCODED=$(jq -rn --arg q "$Q" '$q|@uri')
@@ -37,24 +35,27 @@ TOTAL=$(curl -s \
 
 echo "Commits in the last 24 h across all repos: $TOTAL"
 
-# Validate API response
+# 1.1. Validate API response
 if ! [[ "$TOTAL" =~ ^[0-9]+$ ]]; then
   echo "Error: Failed to fetch total commit contributions from GitHub API."
   exit 1
 fi
 
+# 1.2. Sanity check
 echo "Sanity check"
 echo "Current branch:"
 git branch
 echo "Current commit:"
 git log -1
 
+# --------------------------------------------
 # 2. Create mirror commits
 echo "Creating $TOTAL mirror commits..."
 for ((i=0;i<TOTAL;i++)); do
-  git commit --allow-empty -m "< mirror work commits $i >"
+  git commit --allow-empty -m "< mirror work commits $((i+1)) >"
 done
 
+# --------------------------------------------
 # 3. Push changes
 echo "Pushing changes to main..."
 git push origin HEAD:main --force-with-lease || {
